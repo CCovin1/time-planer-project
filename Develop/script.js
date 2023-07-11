56eba573e64f3afbc9dc160e8c993c86d59edbe5
CCovin1 <caleb.covin@gmail.com>
2023-07-11 15:25:46 -0500

    added the function to show if its past present or current hour

--- Develop/script.js
+++ Develop/script.js
@@ -5,4 +5,20 @@ $(function () {
     $('#currentDay').text(currentDateTime);
   }
 
-}
+  // Function to compare the current hour with the hour of each time block
+  function updateHourStatus() {
+    var currentHour = dayjs().hour();
+
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass('present future').addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
